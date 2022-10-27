5ceb75e609beab34d4e737174fa8ddb567b17c72
smilyidiot <vinayakrachha@gmail.com>
2022-10-27 15:14:10 +0000

    react router

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,77 @@
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import './index.css'
+
+// const blogData = {
+//   title: 'Blog Name',
+//   imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-3-img.png',
+//   avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+//   author: 'Author Name',
+//   content:
+//     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+// }
+
+class BlogItemDetails extends Component {
+  state = {blogData: {}, spinner: true}
+
+  componentDidMount() {
+    this.getBlogsItemData()
+  }
+
+  getBlogsItemData = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+    const updatedData = {
+      title: data.title,
+      imageUrl: data.image_url,
+      avatarUrl: data.avatar_url,
+      author: data.author,
+      topic: data.topic,
+    }
+
+    this.setState({blogData: updatedData, spinner: false})
+
+    console.log(data)
+    console.log(updatedData)
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogData} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogData
+    return (
+      <div className="blog-info">
+        <h2 className="blog-details-title">{title}</h2>
+
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const {spinner} = this.state
+    return (
+      <div className="blog-container">
+        {spinner ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          this.renderBlogItemDetails()
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogItemDetails
